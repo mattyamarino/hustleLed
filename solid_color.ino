f7e2285cbb0833d80a4a7e3f4842ec15a0c0fd54
Matt Yamarino <myamarino@veracode.com>
2022-03-15 15:31:30 -0500

    finished brightness and solid color settings

--- solid_color.ino
+++ solid_color.ino
@@ -14,7 +14,8 @@ IRrecv irrecv(receiver);     // create instance of 'irrecv'
 decode_results results;      // create instance of 'decode_results'
 
 bool ON = false;
-int BRIGHTNESS = 100;
+int BRIGHTNESS_INDEX = 2;
+int BRIGHTNESS_ARRAY[] = {10, 45, 80, 150, 250};
 CRGB leds[NUM_LEDS];
 
 
@@ -22,8 +23,8 @@ CRGB leds[NUM_LEDS];
 void setup() {
   pinMode(buttonApin, INPUT_PULLUP);  
   
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS_ARRAY[BRIGHTNESS_INDEX]);
 
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
@@ -44,40 +45,61 @@ void loop() {
   if (digitalRead(buttonApin) == LOW){
     turnOnOff();
     delay(500);
-
   }
-
-
 
 }
 
 void turnOnOff() {
   if (ON == false)
     {
-      fill_solid( leds, NUM_LEDS, CRGB::Blue);
+      fill_solid( leds, NUM_LEDS, CRGB::Teal );
       ON = true;
       FastLED.show();
     } else if (ON == true) {
       ON = false;
-      BRIGHTNESS = 100;
-      FastLED.setBrightness(BRIGHTNESS);
+      BRIGHTNESS_INDEX = 2;
+      FastLED.setBrightness(BRIGHTNESS_ARRAY[BRIGHTNESS_INDEX]);
       FastLED.clear();
       FastLED.show();  
     }
 }
 
+void setSolidBlue() {
+  fill_solid( leds, NUM_LEDS, CRGB::Blue);
+  ON = true;
+  FastLED.show();  
+}
+
+void setSolidOrange() {
+  fill_solid( leds, NUM_LEDS, CRGB::Orange);
+  ON = true;      
+  FastLED.show();  
+}
+
+void setSolidTeal() {
+  fill_solid( leds, NUM_LEDS, CRGB::Teal);
+  ON = true;      
+  FastLED.show();  
+}
+
+void setRainbow() {
+  fill_rainbow(leds, NUM_LEDS, 0, 255/NUM_LEDS);
+  ON = true;      
+  FastLED.show(); 
+}
+
 void increaseBrightness() {
-  if(BRIGHTNESS < 226) {
-    BRIGHTNESS += 30;
-    FastLED.setBrightness(BRIGHTNESS);
+  if(BRIGHTNESS_INDEX < 4) {
+    BRIGHTNESS_INDEX++;
+    FastLED.setBrightness(BRIGHTNESS_ARRAY[BRIGHTNESS_INDEX]);
     FastLED.show();
   }
 }
 
 void decreaseBrightness() {
- if(BRIGHTNESS  > 29) {
-    BRIGHTNESS -= 30;
-    FastLED.setBrightness(BRIGHTNESS);
+ if(BRIGHTNESS_INDEX > 0) {
+    BRIGHTNESS_INDEX--;
+    FastLED.setBrightness(BRIGHTNESS_ARRAY[BRIGHTNESS_INDEX]);
     FastLED.show();
  }
 }
@@ -89,8 +111,8 @@ void translateIR(){
 
   {
   case 0xFFA25D: Serial.println("POWER"); turnOnOff(); break;
-  case 0xFFE21D: Serial.println("VOL STOP"); break;
-  case 0xFF629D: Serial.println("MODE"); break;
+  case 0xFFE21D: Serial.println("VOL STOP");  break;
+  case 0xFF629D: Serial.println("MODE");  break;
   case 0xFF22DD: Serial.println("PAUSE");    break;
   case 0xFF02FD: Serial.println("FAST BACK");    break;
   case 0xFFC23D: Serial.println("FAST FORWARD");   break;
@@ -100,11 +122,11 @@ void translateIR(){
   case 0xFF9867: Serial.println("RETURN");    break;
   case 0xFFB04F: Serial.println("USB SCAN");    break;
   case 0xFF6897: Serial.println("0");    break;
-  case 0xFF30CF: Serial.println("1");    break;
-  case 0xFF18E7: Serial.println("2");    break;
-  case 0xFF7A85: Serial.println("3");    break;
+  case 0xFF30CF: Serial.println("1");   setSolidBlue(); break;
+  case 0xFF18E7: Serial.println("2");   setSolidOrange(); break;
+  case 0xFF7A85: Serial.println("3");   setSolidTeal(); break;
   case 0xFF10EF: Serial.println("4");    break;
-  case 0xFF38C7: Serial.println("5");    break;
+  case 0xFF38C7: Serial.println("5");   setRainbow(); break;
   case 0xFF5AA5: Serial.println("6");    break;
   case 0xFF42BD: Serial.println("7");    break;
   case 0xFF4AB5: Serial.println("8");    break;
@@ -116,5 +138,5 @@ void translateIR(){
 
   }// End Case
 
-  delay(500); // Do not get immediate repeat
+  delay(350); // Do not get immediate repeat
 }
